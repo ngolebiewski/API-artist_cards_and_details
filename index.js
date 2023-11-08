db3bfbe45c6cacc70b6228599ab1ba111f2c2824
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-08 15:40:13 -0500

    js: rendered artist cards, added css style

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-//make a list of recipes
 const main = document.querySelector(`main`)
-const apiBaseURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-ft-sf-helpdesk/`
+const baseURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-ft-sf-helpdesk/`
+
+const state = {
+  artistData: []
+}
+
+const artistConstructor = async () =>{
+  try{ 
+    const response = await fetch(`${baseURL}artists`);
+    const JSONresponse = await response.json();
+   //ADD IN AN ERROR SITUATION
+    state.artistData = JSONresponse.data;
+  } catch (error) {
+    console.log(error);
+  }
+  renderCards();
+}
+
+const renderCards = () => {
+  const artistCardArray = state.artistData.map(artistInfo => {
+    return `
+    <content>
+    <img src=${artistInfo.imageUrl} width=100%%>
+    <h2>${artistInfo.name}</h2>
+    </content>
+    `
+  })
+  
+  artistCardArray.forEach((artistCardInnerHTML, index) => {
+    const artCard = document.createElement(`article`);
+    artCard.id = (index);
+    main.append(artCard);
+    artCard.innerHTML=artistCardInnerHTML
+    console.log(artCard)
+  } )
+}
+
+artistConstructor();
